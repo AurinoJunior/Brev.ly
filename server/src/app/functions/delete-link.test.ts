8faa6c4042884f76afe217f1aa999e94531b7fe7
Aurino Geraldo <aurinoaj@gmail.com>
2025-06-24 17:49:41 -0300

    test: delete link

---
+++ server/src/app/functions/delete-link.test.ts
@@ -0,0 +1,29 @@
+import { db } from "@/infra/db"
+import { links } from "@/infra/db/schemas/links"
+import { eq } from "drizzle-orm"
+import { describe, expect, it } from "vitest"
+import { LinkNotFound } from "../errors/link-not-found"
+import { createLink } from "./create-link"
+import { deleteLink } from "./delete-link"
+
+describe("delete link", () => {
+  it("should be able to delete a link", async () => {
+    const mockURL = "http://www.site-muito-legal.com.br/"
+
+    const sut = await createLink({
+      url: mockURL,
+    })
+
+    await deleteLink({ id: sut.id })
+
+    const result = await db.select().from(links).where(eq(links.id, sut.id))
+
+    expect(result).toHaveLength(0)
+  })
+
+  it("should not be able to delete a not found link", async () => {
+    await expect(deleteLink({ id: "not-found-id" })).rejects.toThrow(
+      LinkNotFound
+    )
+  })
+})
