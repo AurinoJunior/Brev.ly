a3996dd0ddb26e832d31f3cb9739334ee8db6472
Aurino Geraldo <aurinoaj@gmail.com>
2025-06-25 09:49:06 -0300

    test: get original link

---
+++ server/src/app/functions/get-original-link.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from "vitest"
+import { LinkNotFound } from "../errors/link-not-found"
+import { createLink } from "./create-link"
+import { getOriginalLink } from "./get-original-link"
+
+describe("get original link", () => {
+  it("should be able get original link", async () => {
+    const mockOriginalLink = "http://www.site-muito-legal.com.br/original-link"
+
+    const newLink = await createLink({
+      url: mockOriginalLink,
+    })
+
+    const sut = await getOriginalLink({
+      shortURL: newLink.shortURL,
+    })
+
+    expect(sut.originalURL).toBe(mockOriginalLink)
+  })
+
+  it("should not be able get a original link with not found shortURL", async () => {
+    await expect(getOriginalLink({ shortURL: "not-found-id" })).rejects.toThrow(
+      LinkNotFound
+    )
+  })
+})
