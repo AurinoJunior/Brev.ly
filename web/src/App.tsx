30dba341063fe119f8c793e5b30efcdea227e0a5
Aurino Geraldo <aurinoaj@gmail.com>
2025-07-18 17:45:15 -0300

    feat: config tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,11 @@
 export const App = () => {
-  return <h1 className="font-sans text-xl text-danger">Brev.ly</h1>
+  return (
+    <main className="bg-gray-200 w-full h-screen">
+      <div className="flex flex-col items-center justify-center h-full px-3">
+        <div className="bg-white p-6 rounded-lg w-full max-w-[380px]">
+          <h2 className="text-lg font-bold">Novo link</h2>
+        </div>
+      </div>
+    </main>
+  )
 }
